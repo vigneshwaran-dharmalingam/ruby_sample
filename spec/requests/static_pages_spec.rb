61de2a46f5ecddc451d80422e30232806b6bb0fa
vigneshwaran-dharmalingam <vigneshwaran.dharmalingam@freshworks.com>
2018-06-27 17:45:06 +0530

    chap 5 added and 6 started

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,12 +8,6 @@ describe "Static pages" do
       visit '/static_pages/home'
       page.should have_selector('h1', :text => 'Sample App')
     end
-
-    it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      page.should have_selector('title',
-                                :text => "Ruby on Rails Tutorial Sample App | Home")
-    end
   end
 
   describe "Help page" do
@@ -22,25 +16,13 @@ describe "Static pages" do
       visit '/static_pages/help'
       page.should have_selector('h1', :text => 'Help')
     end
-
-    it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('title',
-                                :text => "Ruby on Rails Tutorial Sample App | Help")
-    end
-  end
+end
 
   describe "About page" do
 
     it "should have the h1 'About Us'" do
       visit '/static_pages/about'
       page.should have_selector('h1', :text => 'About Us')
-    end
-
-    it "should have the title 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector('title',
-                                :text => "Ruby on Rails Tutorial Sample App | About Us")
     end
   end
 
@@ -51,11 +33,6 @@ describe "Static pages" do
       page.should have_selector('h1', text: 'Contact')
     end
 
-    it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
-      page.should have_selector('title',
-                                text: "Ruby on Rails Tutorial Sample App | Contact")
-    end
   end
 
 end
